359c787de437adf0eccaedc7c2aaed0cc60e0e2f
oyvindhg <oyvindhg@gmail.com>
2021-04-11 17:02:23 +0900

    Added simple personal info

---
+++ src/HomePage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+    return (
+        <div className="HomePage">
+            <div>
+                Øyvind Harding Gulbrandsen
+            </div>
+            <div>
+                Software developer
+            </div>
+            <a className="LinkedIn-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                LinkedIn
+            </a>
+        </div>
+    );
+}
+
+export default HomePage;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HomePage from './HomePage';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
